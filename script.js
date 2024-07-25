411abf8a71b804fd16623eb1f7f442a8637cd09f
varshavenkatesh18 <varshini.hv@gmail.com>
2024-07-25 18:05:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Example JavaScript code (you can add more functionality as needed)
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM loaded');
+});
